eea1ae53fba4be6b6909b7875a036ee1cee4e0ce
Tom Noonan II <root@tjnii.com>
2013-09-05 15:11:55 -0500

    Added RHEL support

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,39 @@
 class miscpackages {
-   package { ['curl',
-   	      'dnsutils',
-              'arping',
-              'tcpdump',
-              'telnet',
-              'traceroute',
-              'netcat-traditional',
-              'pciutils',
-              'usbutils',
-              'strace',
-              ]:
-                ensure => installed,
-   }
-
-   # Packages that cause problems
-   package { ['mpt-status',
-              'bash-completion',
-              ]:
-                ensure => absent,
-   }
-                   
+  case $operatingsystem {
+    debian, ubuntu: {
+      package { ['dnsutils',
+                 'arping',
+                 'netcat-traditional',
+                 ]:
+                   ensure => installed,
+      }
+    }
+    redhat, cent: {
+      package { ['bind-utils',
+                 'iputils',
+                 'nc',
+                 ]:
+                   ensure => installed,
+      }
+    }
+  }
+    
+  package { ['curl',
+             'tcpdump',
+             'telnet',
+             'traceroute',
+             'pciutils',
+             'usbutils',
+             'strace',
+             ]:
+               ensure => installed,
+  }
+  
+  # Packages that cause problems
+  package { ['mpt-status',
+             'bash-completion',
+             ]:
+               ensure => purged,
+  }
+  
 }
